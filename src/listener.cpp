a9cb6261456721efe85f80c271d0a4c0bb3f564a
lufan <fan.lu@agvsz.com>
2020-12-21 17:56:59 +0800

    test publisher and subscriber

--- src/listener.cpp
+++ src/listener.cpp
@@ -2,8 +2,10 @@
 #include "std_msgs/String.h"
 
 /**
+ *test
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
+
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
--- src/talker.cpp
+++ src/talker.cpp
@@ -21,6 +21,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "talker");
 
   /**
+   *test
    * NodeHandle is the main access point to communications with the ROS system.
    * The first NodeHandle constructed will fully initialize this node, and the last
    * NodeHandle destructed will close down the node.
